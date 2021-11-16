3633b03bb48661acbe0980a50d507dc553a2065d
hernanruscica <cesarhernanruscica@gmail.com>
2021-11-16 12:32:14 -0300

    html basic structure for the buttons and screen

---
+++ app/app.js
@@ -0,0 +1,7 @@
+import { sum, subt, mul, div } from "./modules/math.js";
+
+let val01 = 7, val02 = 2;
+console.log(`La suma entre ${val01} y ${val02} es ${sum(val01, val02)}`);
+console.log(`La resta entre ${val01} y ${val02} es ${subt(val01, val02)}`);
+console.log(`La multiplicacion entre ${val01} y ${val02} es ${mul(val01, val02)}`);
+console.log(`La division entre ${val01} y ${val02} es ${div(val01, val02)}`);
---
+++ app/modules/math.js
@@ -0,0 +1,12 @@
+export function sum(op01, op02){
+    return op01 + op02;
+}
+export function subt(op01, op02){
+    return op01 - op02;
+}
+export function mul(op01, op02){
+    return op01 * op02;
+}
+export function div(op01, op02){
+    return op01 / op02;
+}
